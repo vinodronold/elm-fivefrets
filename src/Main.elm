ec1776ca53bce80d78d408f7fc9faea365cc1168
Vinod Ronold <vinodronoldbe@gmail.com>
2018-03-10 14:13:33 -0500

    Init

---
+++ src/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "fivefrets"
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
